5684a4145246b6a8a50cc90d3a376b1fde4cccca
gabrielecrestini <crestinigabriele54@gmail.com>
2025-10-23 19:28:12 +0200

    Optimize bundle size and prepare for deploy

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/crestinigabriele54/credpulse/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/0a86735c6520d94f-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/crestinigabriele54/credpulse/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2","static/media/0a86735c6520d94f-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
